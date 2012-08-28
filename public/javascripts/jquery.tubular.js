978c2128185ea6e42e7f74b911f39a52dec05f56
Ryder Moody <rydthemoodster@gmail.com>
2012-08-27 23:43:25 -0400

    Rolled my own fullscreen YT player since the other one sucked

--- public/javascripts/jquery.tubular.js
+++ public/javascripts/jquery.tubular.js
@@ -8,60 +8,82 @@
 |* Thanks,
 |* Sean */
 
-// default params
-var videoWidth = 853; // needed a default value -- this is overwritten almost immediately
-var videoRatio = 16/9; // either 4/3 or 16/9 -- tweak as needed
-var defaultDiv = 'wrapper';
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-jQuery.fn.tubular = function(videoId,wrapperId) {
-	wrapperId = (typeof(wrapperId) == undefined) ? 'wrapper' : wrapperId;
-	t = setTimeout("resizePlayer()",1000); // this is hacky, but i couldn't figure out why the yt player was behind
+
+jQuery.fn.tubular = function(videoId) {
 
 	jQuery('html,body').css('height','100%');
 	jQuery('body').prepend('<div id="yt-container" style="overflow: hidden; position: fixed; z-index: 1;"><div id="ytapiplayer">You need Flash player 8+ and JavaScript enabled to view this video.</div></div><div id="video-cover" style="position: fixed; width: 100%; height: 100%; z-index: 2;"></div>');
-	jQuery('#' + wrapperId).css({position: 'relative', 'z-index': 99});
 	
 	// initiailize vars
-	var ytplayer = 0;
-	var pageWidth = 0;
-	var pageHeight = 0;
+  var videoWidth = 853; // needed a default value -- this is overwritten almost immediately
+  var videoRatio = 16/9; // either 4/3 or 16/9 -- tweak as needed
 	var videoHeight = videoWidth / videoRatio;
 	var duration;
 
 	// iframe embedded yt player
-	var iframe = '<iframe id="myytplayer" width="' + videoWidth + '" height="' + videoHeight + '" src="http://www.youtube.com/embed/' + videoId + '?autoplay=1&controls=0&modestbranding=1&showinfo=0&version=3&wmode=transparent&loop=1&playlist=' + videoId + '" frameborder="0" allowfullscreen></iframe>';
+	var iframe = '<iframe id="player" width="' + videoWidth + '" height="' + videoHeight + '" src="http://www.youtube.com/embed/' + videoId + '?autoplay=1&controls=0&modestbranding=1&showinfo=0&version=3&wmode=transparent&loop=1&playlist=' + videoId + '" frameborder="0" allowfullscreen></iframe>';
 	
-	/*
-
-	// swfobject embed yt player
-	var params = { allowScriptAccess: "always", wmode: "transparent" };
-	var atts = { id: "myytplayer" };
-	swfobject.embedSWF("http://www.youtube.com/v/" + videoId + "?enablejsapi=1&playerapiid=ytplayer&loop=1", "ytapiplayer", videoWidth, videoHeight, "8", null, null, params, atts);
-	
-	*/
-
 	jQuery('#ytapiplayer').html(iframe);
 	
 	jQuery(window).resize(function() {
 		resizePlayer();
 	});
-	
+
+  function resizePlayer() {
+    var newWidth = jQuery(window).width(); // original page width
+    var newHeight = jQuery(window).height(); // original page height
+    jQuery('#yt-container, #video-cover').width(newWidth).height(newHeight);
+    if (newHeight > newWidth / videoRatio) { // if window ratio becomes taller than video
+      newWidth = newHeight * videoRatio; // overflow video to sides instead of bottom
+    }
+    jQuery('#player').width(newWidth).height(newWidth/videoRatio);
+  }
+  resizePlayer();
+    
 	return this;
 }
 
-// player calls this function on ready
-function onYouTubePlayerReady(playerId) {
-	ytplayer = document.getElementById("myytplayer");
-	ytplayer.setPlaybackQuality('high');
-	ytplayer.mute();
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      },
+      playerVars:{
+        start: 3,
+        rel: 0
+      }
+  });
 }
 
-function resizePlayer() {
-	var newWidth = jQuery(window).width(); // original page width
-	var newHeight = jQuery(window).height(); // original page height
-	jQuery('#yt-container, #video-cover').width(newWidth).height(newHeight);
-	if (newHeight > newWidth / videoRatio) { // if window ratio becomes taller than video
-		newWidth = newHeight * videoRatio; // overflow video to sides instead of bottom
-	}
-	jQuery('#myytplayer').width(newWidth).height(newWidth/videoRatio);
+function onPlayerReady(event) {
+  var yt = event.target;
+  yt.setPlaybackQuality('highres');
+  yt.target.playVideo();
 }
+
+
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    done = true;
+  }
+}
+
+$(document).ready(function(){
+  $('#videoPause').click(function(){
+    $this = $(this);
+    if ($this.hasClass('paused')){
+      player.playVideo();
+    } else {
+      player.pauseVideo();
+    }
+    $(this).toggleClass('paused');
+  });
+});
