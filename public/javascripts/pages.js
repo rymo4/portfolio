545f93110fe3ef11fdaf3b2caae2e49278e1fa4a
Ryder Moody <rydthemoodster@gmail.com>
2012-12-13 16:16:45 -0500

    Uses imagesLoaded for cache issues

--- public/javascripts/pages.js
+++ public/javascripts/pages.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){  
+
   $('.go2').click(function(){
     var id = $(this).attr('id');
     $('.page').hide();
     $('.' + id).show();
   });
+
+  $('img.background').imagesLoaded(function($images, $proper, $broken){
+    $images.fadeIn(7000);
+  });
   
 });
-function fadeIn(obj) {
-  $(obj).fadeIn(7000);
-}
+
