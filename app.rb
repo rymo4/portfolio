03838f54394d5ba29d1e54534d38cd9259eddef3
Ryder Moody <rydthemoodster@gmail.com>
2013-01-14 01:31:44 -0500

    Adds more content and better templating system

--- app.rb
+++ app.rb
@@ -4,7 +4,25 @@ class App < Sinatra::Base
   set :haml, :format => :html5, :layout => :layout
 
   get '/' do
+    @projects = {
+      coffee:    { gh: 'rymo4/coffeenated', url: 'http://coffeenated.herokuapp.com', name: 'Coffeenated' },
+      paperless: { url: 'http://www.paperlesspost.com', name: 'Paperless Post' },
+      bayesian:  { name: 'Bayesian Average', gh: 'rymo4/bayesian_average' },
+      civ_net:   { name: 'The Civic Network' },
+      research:  { name: 'Research Match', url: 'http://www.researchmat.ch' },
+      linkful:   { name: 'Linkful', gh: 'rymo4/linkful', url: 'http://www.linkful.herokuapp.com' }
+    }
     haml :index
+  end
+
+  helpers do
+    def partial page, options={}
+       haml page.to_sym, options.merge!(:layout => false)
+    end
+
+    def github info
+      "http://www.github.com/#{info[:gh]}"
+    end
   end
 
   run! if app_file == $0
