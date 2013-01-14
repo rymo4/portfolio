4aff959fe3c020244a4c480da0aa536321b913bd
Ryder Moody <rydthemoodster@gmail.com>
2013-01-13 22:37:01 -0500

    Improve linking system and remove trash files

--- public/javascripts/pages.js
+++ public/javascripts/pages.js
@@ -1,14 +1,20 @@
-$(document).ready(function(){  
+$(document).ready(function(){
 
-  $('.go2').click(function(){
-    var id = $(this).attr('id');
+  var $link = $('[data-page]');
+  $link.attr('href', '#')
+
+  var go_to = function(page){
     $('.page').hide();
-    $('.' + id).show();
+    $('#' + page).show();
+  }
+
+  $link.click(function(){
+    var page = $(this).attr('data-page');
+    go_to(page);
   });
 
   $('img.background').imagesLoaded(function($images, $proper, $broken){
     $images.fadeIn(7000);
   });
-  
 });
 
