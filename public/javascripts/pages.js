d541911eadfa3953535616dd6a7edd27b520b76e
Ryder Moody <rydthemoodster@gmail.com>
2013-10-10 13:35:27 -0400

    Better tracking

--- public/javascripts/pages.js
+++ public/javascripts/pages.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
-
+  var track_link = function(type, target){
+    if (window.location.href.indexOf('localhost') !== -1){
+      console.log("Ignoring track event");
+      //return;
+    }
+    if (type === 'internal'){
+      mixpanel.track("view", {type: type, target: target});
+    } else {
+      mixpanel.track_link("view", {type: type, target: target});
+    }
+  };
   var $link = $('[data-page]');
   $link.attr('href', '#')
+  $('a:not([data-page])').click(function(l){
+    var href = $(this).attr('href');
+    if (href !== '#'){
+      track_link("external", href);
+    }
+  });
 
   var go_to = function(page){
-    mixpanel.track("View " + page);
+    track_link("internal", page);
     $('.page').hide();
     $('#' + page).show();
   }
